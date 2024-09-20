17435136bdca10e86d996f79cf28350c0705f0ba
sonnycalcr <sonnycalcr@gmail.com>
2024-09-21 00:58:51 +0800

    完善转换规则

--- shuangpin.py
+++ shuangpin.py
@@ -1,5 +1,18 @@
 """
 小鹤双拼 + 辅助码
+
+零声母举例：
+eng：鞥
+ng 在大陆的词典里面读的是 ng，这个应该怎么映射到双拼呢？
+
+教育部的pdf：http://www.moe.gov.cn/ewebeditor/uploadfile/2015/03/02/20150302165814246.pdf
+
+关于 u 和 ü 的一点说明：
+
+nue 这个全拼不应该存在，应该是 nve，也就是 nüe
+同理，还有 lue 和 lve 之间的问题
+
+j q x 要挖掉鱼的两点
 """
 
 import os.path
@@ -9,6 +22,9 @@ class XiaoheShuangpin:
     def __init__(self) -> None:
         # 声母映射
         self.sm_keymaps = {"sh": "u", "ch": "i", "zh": "v"}
+        # 零声母映射
+        self.zero_sm_keymaps = {"a": "aa", "ai": "ai", "ao": "ao", "ang": "ah", "e": "ee", "ei": "ei", "en": "en", "eng":  "eg", "er": "er", "o": "oo", "ou": "ou"}
+        self.zero_sm_keymaps_reversed = {v: k for k, v in self.zero_sm_keymaps.items()}
         # 韵母映射
         self.ym_keymaps = {
             "iu": "q",
@@ -56,27 +72,19 @@ class XiaoheShuangpin:
                 cur_line = each_line.strip()
                 if cur_line not in self.quanpin_tbl:
                     self.quanpin_tbl.add(cur_line)
-                else:
-                    print(cur_line)
-        print(len(self.quanpin_tbl))
-
 
     def cvt_single_pinyin_to_sp(self, pinyin: str) -> str:
         """
         把单个拼音(全拼)转换为小鹤双拼
         """
-        if len(pinyin) == 1:
-            # 如果是 n、a 这种单声母或者韵母，且，是单字母的拼音，虽然 n 比较特殊，但是，这个因为 niao 本身所拥有的字数并不算多，所以可以并在一起
-            pinyin = pinyin * 2
+        if pinyin in self.zero_sm_keymaps.keys():
+            return self.zero_sm_keymaps[pinyin]
         elif len(pinyin) == 2:
             # 双字母保持全拼方式，如：li，xi，wu，ng，an
             pinyin = pinyin
         elif len(pinyin) > 2:
-            # 如果是 ang 这种单韵母拼音，且为三字母，规则是首字母加韵母所在键
-            if pinyin in self.sm_keymaps.keys():
-                pinyin = pinyin[0] + self.sm_keymaps[pinyin]
             # 如果声母是两个字母
-            elif pinyin[:2] in self.sm_keymaps.keys():
+            if pinyin[:2] in self.sm_keymaps.keys():
                 pinyin = self.sm_keymaps[pinyin[:2]] + self.ym_keymaps[pinyin[2:]]
             # 声母是单字母
             else:
@@ -86,7 +94,10 @@ class XiaoheShuangpin:
     def cvt_single_sp_to_pinyin(self, sp_str: str) -> list[str]:
         """
         把小鹤双拼转换为拼音(全拼)
+        目前针对 402 个拼音是一对一的方案
         """
+        if sp_str in self.zero_sm_keymaps.values():
+            return [self.zero_sm_keymaps_reversed[sp_str]]
         if (len(sp_str) != 2):
             return []
         sm: str = ""
@@ -102,10 +113,27 @@ class XiaoheShuangpin:
             if sp_str[1] == value:
                 ym_list.append(key)
         for each_ym in ym_list:
-            res.append(sm + each_ym)
+            if (sm + each_ym) in self.quanpin_tbl:
+                res.append(sm + each_ym)
         return res
 
 
 if __name__ == "__main__":
     xiaohe_shuangpin = XiaoheShuangpin()
-    print(xiaohe_shuangpin.cvt_single_sp_to_pinyin("ul"))
+    # print(xiaohe_shuangpin.cvt_single_sp_to_pinyin("ul"))
+    cnt = 0
+    ulpb_cnt = 0
+    ul_set = set()
+    for each_quanpin in xiaohe_shuangpin.quanpin_tbl:
+        # print(each_quanpin, end=": ")
+        cur_ul = xiaohe_shuangpin.cvt_single_pinyin_to_sp(each_quanpin)
+        # print(cur_ul)
+        cnt += 1
+        if (cur_ul not in ul_set):
+            ul_set.add(cur_ul)
+            ulpb_cnt += 1
+        else:
+            print(each_quanpin, end=": ")
+            print(cur_ul)
+    print(cnt)
+    print(ulpb_cnt)
