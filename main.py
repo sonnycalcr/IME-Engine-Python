1801578ef6cbb635262a9ff473d9cc7b57c8c713
sonnycalcr <sonnycalcr@gmail.com>
2024-09-25 00:49:26 +0800

    some input test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import readchar
+import curses
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
@@ -8,14 +9,20 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 from cloud_api.cloud_ime import CloudinputApi
 from xiaohe_sp.xiaohe_sp import XiaoheShuangpin
 
+stdscr = curses.initscr()
 
 if __name__ == "__main__":
     pinyin_str = ""
     while True:
         cur_char = readchar.readchar()
-        pinyin_str += cur_char
-        print(pinyin_str)
+        if cur_char >= 'a' and cur_char <= 'z':
+            pinyin_str += cur_char
+        elif cur_char == readchar.key.BACKSPACE:
+            if len(pinyin_str) > 0:
+                pinyin_str = pinyin_str[:-1]
+        # print(pinyin_str, end='\r')
+        sys.stdout.write("%s\r" % "                                                                              ")
+        sys.stdout.write("%s\r" % pinyin_str)
+        sys.stdout.flush()
         if (cur_char == readchar.key.CTRL_C):
             break
-
-        
